299db5977a41d0328cb1adfa372c1f25a78dba0c
headB_macbook_pro <775121173@qq.com>
2018-10-27 16:29:11 +0800

    通过配置文件,进行一些初始参数的加载

--- test_01.py
+++ test_01.py
@@ -22,7 +22,10 @@ class IndexHandler(tornado.web.RequestHandler):
 if __name__ == '__main__':
     #添加下面这条语句的话,会转换 启动时候,会转换那些在给模块传递的参数,
     #举例,python manage -h cc -b ff 这些.
-    tornado.options.parse_command_line()
+    # tornado.options.parse_command_line()
+    tornado.options.parse_config_file("config.py")
+    
+
 
     #开启debug模式
     #app = tornado.web.Application([(r"/",IndexHandler)],debug=True)
